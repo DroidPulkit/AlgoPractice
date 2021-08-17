50a31312d480aab4c67f4fdedd9658d6c3d54a3d
Pulkit <pulkitkumar190@gmail.com>
2021-08-17 07:58:25 -0400

    Update

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -3,13 +3,21 @@ import java.util.List;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        System.out.println(findPrimeNumberOptimizationThree(100000000));
+        //System.out.println(findPrimeNumberOptimizationThree(100000000));
+        findPrimeNumberOptimizationThree(1);
+
+        try {
+            findPrimeNumberOptimizationThree(100000000);
+        } catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
     }
 
 
     //This is the brute force method
     //This method sucks as we add more and more zeros
-    //Kind of big O(n square)
+    //Big O(n)
     static List<Integer> findPrimeNumber(int size)
     {
         List<Integer> listOfPrimeNumbers = new ArrayList<>();
@@ -33,7 +41,7 @@ public class PrimeNumber {
 
     //This method is cutting 2nd for loop, where we check if counter value is more than 2,
     // then we just know it is not prime, and move to next number
-    //Almost similar Kind of big O(n square), there is difference but not something which I can quantify I guess
+    //Big O(n)
     static List<Integer> findPrimeNumberOptimizationOne(int size)
     {
         List<Integer> listOfPrimeNumbers = new ArrayList<>();
@@ -61,6 +69,7 @@ public class PrimeNumber {
 
     //This method add optimization on top of above, where we cut the 2nd for loop with sqrt(number),
     // for which we need to find if the given number was prime or not
+    //Big O(sqrt(N))
     static List<Integer> findPrimeNumberOptimizationTwo(int size)
     {
         List<Integer> listOfPrimeNumbers = new ArrayList<>();
@@ -88,32 +97,68 @@ public class PrimeNumber {
         return listOfPrimeNumbers;
     }
 
-    //Sieve Algorithm,
-    //Big O(log(n))
-    static List<Integer> findPrimeNumberOptimizationThree(int size)
+    //This method add optimization on top of above, where we cut the 2nd for loop with sqrt(number),
+    // for which we need to find if the given number was prime or not
+    //Big O(sqrt(N))
+    static List<Integer> findPrimeNumberOptimizationTwoMore(int size)
     {
-        boolean compositeNumberList[] = new boolean[size + 1];
-        for(int i = 2; (i * i) <= size; i++)
+        List<Integer> listOfPrimeNumbers = new ArrayList<>();
+        listOfPrimeNumbers.add(2);
+        listOfPrimeNumbers.add(3);
+        for(int i = 4; i <= size; i++)
         {
-            if (compositeNumberList[i])
+            boolean addToList = true;
+            //we know we already have 2 factors 1 and the number itself,
+            // so any more factors will make it not prime
+            for(int j = 2; j * j <= i; j++)
             {
-                break;
+                if (i%j == 0)
+                {
+                    addToList = false;
+                    break;
+                }
             }
-            for(int j = 2; i * j <= size; j++)
+            if(addToList)
             {
-                compositeNumberList[i * j] = true;
+                listOfPrimeNumbers.add(i);
             }
         }
-        List<Integer> integersList = new ArrayList<>();
-        integersList.add(2);
-        integersList.add(3);
-        for (int i = 4; i < compositeNumberList.length; i++)
+        return listOfPrimeNumbers;
+    }
+
+    //Sieve Algorithm,
+    //Big O(log(n))
+    static void findPrimeNumberOptimizationThree(int size)
+    {
+        //All boolean default value is false
+        //we are assuming here, all integers are prime, and it starts from 2
+        boolean[] compositeNumberList = new boolean[size - 1];
+        //This for loop purpose is to determine the multiples of number starting from 2
+        for(int i = 2; (i * i) <= size; i++)
+        {
+            if (compositeNumberList[i - 2])
+            {
+                continue;
+            }
+            //Here we find multiples and mark them as composite, but we start from
+            // i * i position, as previous one have to be marked already
+            for(int j = i; i * j <= size; j++)
+            {
+                compositeNumberList[(i * j) - 2] = true;
+            }
+        }
+        //List<Integer> integersList = new ArrayList<>();
+        int len = compositeNumberList.length;
+        int counter = 0;
+        for (int i = 0; i < len; i++)
         {
             if (!compositeNumberList[i])
             {
-                integersList.add(i);
+                ++counter;
+                if (counter%100 == 1) System.out.print(i + 2 + "\n");
+                //integersList.add(i + 2);
             }
         }
-        return integersList;
+        //return integersList;
     }
 }
